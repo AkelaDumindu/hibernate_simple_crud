ebf2aa2806a9d782920e2cd6ad6e5dee79a1a3f0
AkelaDumindu <akeladumindu2000@gmail.com>
2024-10-31 12:59:09 +0530

    find student

--- src/Appinitializer.java
+++ src/Appinitializer.java
@@ -1,21 +1,24 @@
 import entity.Book;
 import entity.Student;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 
 public class Appinitializer {
 
     public static void main(String[] args) {
         Student student = new Student(1,"Akela Dumindu", 95);
-        saveStudent(student);
+//        saveStudent(student);
+        findStudent(1);
     }
 
     private static void saveStudent(Student student){
         try(Session session = HibernateUtil.getSessionFactory().openSession()){
             //save->it return a serializable object, saved object primary key
             //persist, saveOrUpdate
-
+            Transaction transaction = session.beginTransaction();
             long primaryKey = (Long) session.save(student);
+            transaction.commit();
             System.out.println(primaryKey);
         }
     }
@@ -25,6 +28,18 @@ public class Appinitializer {
 
             long primaryKey = (Long) session.save(book);
             System.out.println(primaryKey);
+        }
+    }
+
+    private static void findStudent(long id){
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            Student student =session.find(Student.class,id);
+
+            if (student != null){
+                System.out.println(student.toString());
+            }else {
+                System.out.println("Can\'t find data");
+            }
         }
     }
 
